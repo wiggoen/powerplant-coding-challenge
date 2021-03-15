225d275a2ee28e9e9d74a90101a577afe5dc41b8
TJ <13951333+wiggoen@users.noreply.github.com>
2021-03-15 14:32:43 +0100

    Updated work in progress app

--- app.py
+++ app.py
@@ -4,6 +4,57 @@ import json
 
 app = Flask(__name__)  # Create an instance of the Flask class
 
+'''
+# Tried the bisection method for a simple problem.
+# Found out that it was a bad choice.
+def bisection(f, a, b, eps):
+  fa = f(a)
+  if fa*f(b) > 0:
+    return None, 0
+
+  i = 0  # Iteration counter
+  while b-a > eps:
+    i += 1
+    m = (a+b)/2
+    fm = f(m)
+    if fa*fm <= 0:
+      b = m  # root is in left half of [a,b]
+    else:
+      a = m  # root is in right half of [a,b]
+      fa = fm
+  return m, i
+
+def f(x):
+  return 2*x - 3
+
+a=0 #stations[0]['eff_pmin']
+b=10 #stations[0]['eff_pmax']
+x, i = bisection(f, a, b, eps=1E-5)
+print(x, iter)
+if x is None:
+  print('f(x) does not change sign in [%g, %g]' % (a, b))
+else:
+  print('The root is', x, 'found in', i, 'iterations')
+  print('f(%g)=%g' % (x, f(x)))
+'''
+
+def calculate_production(load, stations):
+  # Implementation of how to calculate production.
+  # Solving a multi-dimensional root problem.
+
+  production = []
+
+  for powerstations in stations:
+    name = powerstations['name']
+
+    # Calculate power production p for each station
+    p = 0
+
+    power_production = {'name': name, 'p': p}
+    production.append(power_production)
+
+  return production
+
 
 def merit_order(data):
   # Payload
@@ -34,12 +85,14 @@ def merit_order(data):
     stations.append(station)
 
   print('--- --- --- --- --- --- --- --- --- ---')
-  print('Load to equal:', load)
-  print('FIND LINEAR EQUATION EQUAL TO LOAD.')
+  print('Payload load to equal:', load)
+  print('SOLVE LINEAR EQUATION EQUAL TO LOAD.')
+  result = calculate_production(load, stations)
+  # Constraints on first powerplant (for testing)
+  #print(stations[0]['eff_pmin'], stations[0]['eff_pmax'])
   print('--- --- --- --- --- --- --- --- --- ---')
 
-  result = json.dumps(stations, indent=2)
-  return result
+  return json.dumps(result, indent=2)
 
 
 @app.route('/')
@@ -63,6 +116,7 @@ def productionplan_response():
 #@app.errorhandler(405)
 #def internal_error(error):
 #    return render_template('405.html'),405
+
 
 #@app.errorhandler(500)
 #def internal_error(error):
