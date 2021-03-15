b765b6ecb12ef21b9d616de456396e3242bd06de
TJ <13951333+wiggoen@users.noreply.github.com>
2021-03-15 11:39:35 +0100

    Updated work in progress app

--- app.py
+++ app.py
@@ -5,9 +5,40 @@ import json
 app = Flask(__name__)  # Create an instance of the Flask class
 
 
-def productionplan_response(data):
-  data = data
-  result = '--- Formatted dictionary ---\n'
+def merit_order(data):
+  # Payload
+  load = data['load']                # Amount of energy in MWh
+  fuels = data['fuels']              # Dictionary of fuels
+  powerplants = data['powerplants']  # List of dictionaries of powerplants
+
+  # Production metrics of fuels
+  gas = fuels['gas(euro/MWh)']            # Euro/MWh
+  kerosine = fuels['kerosine(euro/MWh)']  # Euro/MWh
+  CO2 = fuels['co2(euro/ton)']            # Euro/ton
+  wind = fuels['wind(%)']                 # % (percentage)
+
+  # Powerplants based on production efficiency
+  stations = []
+  for powerstation in powerplants:
+    name = powerstation['name']
+    efficiency = powerstation['efficiency']
+    pmin = powerstation['pmin']
+    pmax = powerstation['pmax']
+    if powerstation['type'] in ['gasfired', 'turbojet']:
+      min_production = pmin * efficiency
+      max_production = pmax * efficiency
+    elif powerstation['type'] is 'windturbine':
+      min_production = pmin * efficiency * wind
+      max_production = pmax * efficiency * wind
+    station = {'name': name, 'eff_pmin': min_production, 'eff_pmax': max_production}
+    stations.append(station)
+
+  print('--- --- --- --- --- --- --- --- --- ---')
+  print('Load to equal:', load)
+  print('FIND LINEAR EQUATION EQUAL TO LOAD.')
+  print('--- --- --- --- --- --- --- --- --- ---')
+
+  result = json.dumps(stations, indent=2)
   return result
 
 
@@ -17,29 +48,25 @@ def index():
 
 
 @app.route('/productionplan', methods=['POST'])
-def test():
+def productionplan_response():
   if request.method == 'POST':
     if request.is_json:
-      data = request.get_json()
-
-      # DO SOMETHING WITH THE DATA!!
-      print(data)
-      result = productionplan_response(data)
-
+      data = request.get_json()  # Dictionary of JSON data
+      result = merit_order(data)
       return result, 200
     else:
-      return 'POST is not JSON.'
+      return 'Error: POST is not JSON.'
   else:
     return 'Error: Method is not POST.'
 
-@app.errorhandler(405)
-def internal_error(error):
-    return render_template('405.html'),405
 
-@app.errorhandler(500)
-def internal_error(error):
-    return render_template('500.html'),500
+#@app.errorhandler(405)
+#def internal_error(error):
+#    return render_template('405.html'),405
 
+#@app.errorhandler(500)
+#def internal_error(error):
+#    return render_template('500.html'),500
 
 
 if __name__ == '__main__':
