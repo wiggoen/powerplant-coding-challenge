7a31244cea286c0b926aeab29b8715ab1f6a96d3
TJ <13951333+wiggoen@users.noreply.github.com>
2021-03-23 17:38:48 +0100

    Updated work in progress app

--- app.py
+++ app.py
@@ -4,58 +4,77 @@ import json
 
 app = Flask(__name__)  # Create an instance of the Flask class
 
-'''
-# Tried the bisection method for a simple problem.
-# Found out that it was a bad choice.
-def bisection(f, a, b, eps):
-  fa = f(a)
-  if fa*f(b) > 0:
-    return None, 0
 
-  i = 0  # Iteration counter
-  while b-a > eps:
-    i += 1
-    m = (a+b)/2
-    fm = f(m)
-    if fa*fm <= 0:
-      b = m  # root is in left half of [a,b]
-    else:
-      a = m  # root is in right half of [a,b]
-      fa = fm
-  return m, i
+def production_range(powerplants, wind):
+  for i in range(len(powerplants)):
+    efficiency = powerplants[i]['efficiency']
+    pmin = powerplants[i]['pmin']
+    pmax = powerplants[i]['pmax']
+    # production based on efficiency
+    min_production = pmin * efficiency
+    max_production = pmax * efficiency
+    if powerplants[i]['type'] in 'windturbine':
+      # production based on available wind
+      min_production *= wind / 100
+      max_production *= wind / 100
+    # add production range
+    powerplants[i].update({'prange': [round(min_production, 1), \
+                                      round(max_production, 1)]})
+  return powerplants
 
-def f(x):
-  return 2*x - 3
 
-a=0 #stations[0]['eff_pmin']
-b=10 #stations[0]['eff_pmax']
-x, i = bisection(f, a, b, eps=1E-5)
-print(x, iter)
-if x is None:
-  print('f(x) does not change sign in [%g, %g]' % (a, b))
-else:
-  print('The root is', x, 'found in', i, 'iterations')
-  print('f(%g)=%g' % (x, f(x)))
-'''
-
-def calculate_production(load, stations):
+def calculate_production(load, powerplants, wind):
   # Implementation of how to calculate production.
   # Solving a multi-dimensional root problem.
 
   # WORK IN PROGRESS!!!
+  # The simple naive solution (hierarchy based)
+  # 1. Wind power (because of cost and efficiency)
+  # 2. Gas (because of cost)
+  # 3. Kerosine (because of cost)
 
-  production = []
+  # Hierarchy based on cost ['windturbine', 'gasfired', 'turbojet']
+  sorted_powerplants = sorted(powerplants, key=lambda k: \
+                              (k['type'] == 'windturbine', \
+                               k['type'] == 'gasfired', \
+                               k['type'] == 'turbojet', \
+                               k['pmax']), reverse=True)
 
-  for powerstations in stations:
-    name = powerstations['name']
+  print('SORTED:\n', json.dumps(sorted_powerplants, indent=2))
 
-    # Calculate power production p for each station
+  powerplant_production = []
+  total_production = 0
+
+  for powerstation in sorted_powerplants:
+    name = powerstation['name']
     p = 0
+    efficiency = powerstation['efficiency']
+    min_production, max_production = powerstation['prange']
 
-    power_production = {'name': name, 'p': p}
-    production.append(power_production)
+    #### TEST if adding more power is more than load.
+    #### If so, add min power - or check how much power and check if it is
+    #### between min and max power for powerplant
 
-  return production
+    if load == total_production:
+      print('LOAD achieved!')
+    elif load > total_production + max_production:
+      total_production += max_production
+      p = max_production
+    elif total_production + min_production < load < total_production + max_production:
+      rest = load - total_production
+      total_production += rest
+      p = rest
+    else:
+      print('Special case....')
+      print('L:', load, ' p_tot:', total_production)
+
+    if total_production < load:
+      print('Load NOT achieved! Need more power!')
+
+    power_production = {'name': name, 'p': round(p/efficiency, 1)}
+    powerplant_production.append(power_production)
+
+  return powerplant_production
 
 
 def merit_order(data):
@@ -68,31 +87,24 @@ def merit_order(data):
   gas = fuels['gas(euro/MWh)']            # Euro/MWh
   kerosine = fuels['kerosine(euro/MWh)']  # Euro/MWh
   CO2 = fuels['co2(euro/ton)']            # Euro/ton
-  wind = fuels['wind(%)']                 # % (percentage)
+  wind = fuels['wind(%)']                 # % (percent)
 
   # Powerplants based on production efficiency
-  stations = []
-  for powerstation in powerplants:
-    name = powerstation['name']
-    efficiency = powerstation['efficiency']
-    pmin = powerstation['pmin']
-    pmax = powerstation['pmax']
-    if powerstation['type'] in ['gasfired', 'turbojet']:
-      min_production = pmin * efficiency
-      max_production = pmax * efficiency
-    elif powerstation['type'] is 'windturbine':
-      min_production = pmin * efficiency * wind
-      max_production = pmax * efficiency * wind
-    station = {'name': name, 'eff_pmin': min_production, 'eff_pmax': max_production}
-    stations.append(station)
+  powerstations = production_range(powerplants, wind)
+
 
   print('--- --- --- --- --- --- --- --- --- ---')
+  #print('POWERPLANTS:\n', powerplants) #-------------------------------------#
+  print('Powerstations:\n', powerstations)
   print('Payload load to equal:', load)
   print('SOLVE LINEAR EQUATION EQUAL TO LOAD.')
-  result = calculate_production(load, stations)
+  result = calculate_production(load, powerstations, wind)
+  #result = 0
   # Constraints on first powerplant (for testing)
   #print(stations[0]['eff_pmin'], stations[0]['eff_pmax'])
   print('--- --- --- --- --- --- --- --- --- ---')
+
+  # TODO: SORT POWER STATIONS BASED ON PRODUCTION BEFORE SENDING THE RESPONSE?
 
   return json.dumps(result, indent=2)
 
@@ -107,6 +119,11 @@ def productionplan_response():
   if request.method == 'POST':
     if request.is_json:
       data = request.get_json()  # Dictionary of JSON data
+
+      # ----------------------------------------------------------------------#
+      #print('PAYLOAD:\n', data)
+      # ----------------------------------------------------------------------#
+
       result = merit_order(data)
       return result, 200
     else:
